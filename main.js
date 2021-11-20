317e0c37ad71af16bf09a419ff8abad52e46e969
jasmijn301 <454893@student.fontys.nl>
2021-11-20 17:18:40 +0100

    api oproepen amenities
    methode gevonden die wel werkt en de link naar de api veranderd

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+// const axios = require('axios');
+
 const url = 'https://opendata.cbs.nl/ODataApi/OData/84799NED/$metadata#Cbs.OData.WebAPI.UntypedDataSet'
 async function getAge() {
     const response = await fetch(url);
     const data = await response.json();
-    const { k_0Tot15Jaar_8, k_15Tot25Jaar_9, k_25Tot45Jaar_10, k_45Tot65Jaar_11, k_65JaarOfOuder_12 } = data;
+    console.log(data);
+    const {
+        k_0Tot15Jaar_8,
+        k_15Tot25Jaar_9,
+        k_25Tot45Jaar_10,
+        k_45Tot65Jaar_11,
+        k_65JaarOfOuder_12
+    } = data;
     console.log(k_0Tot15Jaar_8);
     console.log(k_15Tot25Jaar_9);
     console.log(k_25Tot45Jaar_10);
@@ -13,16 +22,35 @@ async function getAge() {
 
 console.log("hello");
 
-fetch('https://opendata.cbs.nl/ODataApi/OData/84799NED/$metadata#Cbs.OData.WebAPI.UntypedDataSet')
-.then(res => {
-    if (res.ok) {
-        console.log("succes")
-    } else {
-        console.log("not succesful")
-    }
-})
-.then(data => console.log(data))
- 
+// fetch('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filter=WijkenEnBuurten%20eq%20%27BU07722110%27')
+// .then(res => {
+//     console.log(res);
+//     if (res.ok) {
+//         console.log("succes")
+//     } else {
+//         console.log("not succesful")
+//     }
+// })
+// .then(data => console.log(data))
+
+axios.get('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filter=WijkenEnBuurten%20eq%20%27BU07722110%27')
+    .then(res => {
+        const dataset = res.data.value[0];
+        console.log(dataset);
+        console.log(dataset.k_0Tot15Jaar_8);
+
+        const age1 = parseInt(dataset.k_0Tot15Jaar_8);
+        const age2 = parseInt(dataset.k_0Tot15Jaar_8);
+        const age3 = parseInt(dataset.k_0Tot15Jaar_8);
+        const age4 = parseInt(dataset.k_0Tot15Jaar_8);
+        const age5 = parseInt(dataset.k_0Tot15Jaar_8);
+
+
+        // voorbeeld berekening: (age1 + age2) / (A + B + C + D + E)
+  
+    });
+
+
 
 
 
