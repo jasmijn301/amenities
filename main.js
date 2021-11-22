2249ac3da36e49be9d969046ee7348e62098dcec
jasmijn301 <454893@student.fontys.nl>
2021-11-22 15:02:11 +0100

    change of design
    design is changed which also means I need to do a different calculation

--- main.js
+++ main.js
@@ -1,37 +1,6 @@
-// const axios = require('axios');
-
 const url = 'https://opendata.cbs.nl/ODataApi/OData/84799NED/$metadata#Cbs.OData.WebAPI.UntypedDataSet'
-async function getAge() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    const {
-        k_0Tot15Jaar_8,
-        k_15Tot25Jaar_9,
-        k_25Tot45Jaar_10,
-        k_45Tot65Jaar_11,
-        k_65JaarOfOuder_12
-    } = data;
-    console.log(k_0Tot15Jaar_8);
-    console.log(k_15Tot25Jaar_9);
-    console.log(k_25Tot45Jaar_10);
-    console.log(k_45Tot65Jaar_11);
-    console.log(k_65JaarOfOuder_12);
-}
-
 
 console.log("hello");
-
-// fetch('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filter=WijkenEnBuurten%20eq%20%27BU07722110%27')
-// .then(res => {
-//     console.log(res);
-//     if (res.ok) {
-//         console.log("succes")
-//     } else {
-//         console.log("not succesful")
-//     }
-// })
-// .then(data => console.log(data))
 
 axios.get('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filter=WijkenEnBuurten%20eq%20%27BU07722110%27')
     .then(res => {
@@ -40,13 +9,26 @@ axios.get('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filte
         console.log(dataset.k_0Tot15Jaar_8);
 
         const age1 = parseInt(dataset.k_0Tot15Jaar_8);
-        const age2 = parseInt(dataset.k_0Tot15Jaar_8);
-        const age3 = parseInt(dataset.k_0Tot15Jaar_8);
-        const age4 = parseInt(dataset.k_0Tot15Jaar_8);
-        const age5 = parseInt(dataset.k_0Tot15Jaar_8);
+        const age2 = parseInt(dataset.k_15Tot25Jaar_9);
+        const age3 = parseInt(dataset.k_25Tot45Jaar_10);
+        const age4 = parseInt(dataset.k_45Tot65Jaar_11);
+        const age5 = parseInt(dataset.k_65JaarOfOuder_12);
 
+        if (age1 > age2, age3, age4, age5) {
 
-        // voorbeeld berekening: (age1 + age2) / (A + B + C + D + E)
+        }
+        else if (age2 > age1, age3, age4, age5) {
+
+        }
+        else if (age3 > age1, age2, age4, age5) {
+            
+        }
+        else if (age4 > age1, age3, age2, age5) {
+            
+        }
+        else {
+
+        }
   
     });
 
@@ -54,6 +36,3 @@ axios.get('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filte
 
 
 
-// fetch('https://opendata.cbs.nl/ODataApi/OData/84799NED/$metadata#Cbs.OData.WebAPI.UntypedDataSet')
-// .then(res => res.json()) 
-// .then(data => console.log(data))
