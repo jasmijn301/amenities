018ac2f34e3e59074ce60f3a6fe6b630785a5f8f
jasmijn301 <454893@student.fontys.nl>
2021-11-26 17:24:56 +0100

    Start of safety rating
    cleaned up the age part.
    
    I found a way to look at the numbers in the csv file for crimerates, they are not implemented yet.

--- main.js
+++ main.js
@@ -1,40 +1,75 @@
-const url = 'https://opendata.cbs.nl/ODataApi/OData/84799NED/$metadata#Cbs.OData.WebAPI.UntypedDataSet'
+//Show the median age group in the amenities
 
 axios.get('https://opendata.cbs.nl/ODataApi/odata/84799NED/UntypedDataSet?$filter=WijkenEnBuurten%20eq%20%27BU07722110%27')
     .then(res => {
-        const dataset = res.data.value[0];
-        
-        // console.log(dataset);
-        // console.log(dataset.k_0Tot15Jaar_8);
+        const dataAge = res.data.value[0];
 
-        const age1 = parseInt(dataset.k_0Tot15Jaar_8);
-        const age2 = parseInt(dataset.k_15Tot25Jaar_9);
-        const age3 = parseInt(dataset.k_25Tot45Jaar_10);
-        const age4 = parseInt(dataset.k_45Tot65Jaar_11);
-        const age5 = parseInt(dataset.k_65JaarOfOuder_12);
+        const age1 = parseInt(dataAge.k_0Tot15Jaar_8);
+        const age2 = parseInt(dataAge.k_15Tot25Jaar_9);
+        const age3 = parseInt(dataAge.k_25Tot45Jaar_10);
+        const age4 = parseInt(dataAge.k_45Tot65Jaar_11);
+        const age5 = parseInt(dataAge.k_65JaarOfOuder_12);
 
         if (age1 > age2 && age1 > age3 && age1 > age4 && age1 > age5) {
-            console.log(age1);
+            
             document.getElementById("medianAge").innerHTML = "0-15";
         }
         else if (age2 > age1 && age2 > age3 && age2 > age4 && age2 > age5) {
-            console.log(age2);
+            
             document.getElementById("medianAge").innerHTML = "15-25";
         }
         else if (age3 > age1 && age3 > age2 && age3 > age4 && age3 > age5) {
-            console.log(age3);    
+               
             document.getElementById("medianAge").innerHTML = "25-45";
         }
         else if (age4 > age1 && age4 > age2 && age4 > age3 && age4 > age5) {
-            console.log(age4); 
+            
             document.getElementById("medianAge").innerHTML = "45-65";
         }
         else {
-            console.log(age5);
+         
             document.getElementById("medianAge").innerHTML = "65+";
         }
   
     });
+
+// show the safety rating in the amenities
+
+ratingNeighbourhood();
+
+    async function ratingNeighbourhood() {
+        const response = await fetch('crimiIrisbuurt.csv');
+        const data = await response.text();
+      
+
+       const rows = data.split('\n');
+      
+
+       rows.forEach(elt => {
+           const row = elt.split(';');
+   
+        const numbers = row[1];
+      
+        console.log(numbers);
+       })
+
+
+
+
+     };
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
